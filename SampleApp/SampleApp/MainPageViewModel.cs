9172e6de47772f6138f3c498210cfa6e5548992c
Aaron Müller <aaron.mueller@protecdata.ch>
2019-12-05 10:07:37 +0100

    Initial commit.

---
+++ SampleApp/SampleApp/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace SampleApp
+{
+    public class MainPageViewModel
+    {
+        public List<TemplatedObject> TemplatedObjects { get; set; }
+
+        public MainPageViewModel()
+        {
+            TemplatedObjects = new List<TemplatedObject>();
+            for (var i = 0; i < 10; i++)
+            {
+                TemplatedObjects.Add(new TemplatedObject{ Title = $"My Title {i}", Description = "This is a default description."});
+            }
+        }
+    }
+
+    public class TemplatedObject
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
